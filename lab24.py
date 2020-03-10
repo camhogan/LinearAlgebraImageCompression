fb472850d2d3ce467ee0c8de5f76769c4120ed96
Cam Hogan <cameron.j.hogan@gmail.com>
2020-03-10 17:20:58 -0600

    initial commit

---
+++ lab24.py
@@ -0,0 +1,33 @@
+import numpy as NP
+import numpy.linalg as LA
+import imageio as IO
+import matplotlib.pyplot as PLT
+import scipy.misc
+
+F = scipy.misc.face(gray=True)
+
+
+U,S,VT = LA.svd(F)
+
+s=200
+
+Ftest = U[:,:s].dot(NP.diag(S[:s])).dot(VT[:s])
+
+PLT.imshow(Ftest, cmap='gray')
+
+PLT.savefig('compressed_raccoon.png')
+
+def ftest(s):
+  ushape = U[:,:s].shape
+  vtshape = VT[:s].shape
+  sigma = len(S)
+  image = F.shape
+  
+  mem = ushape[0]*ushape[1] + sigma + vtshape[0]*vtshape[1]
+  
+  perc = mem/(image[0]*image[1])
+  
+  return perc
+
+for s in [500, 300, 200, 100, 20, 1]:
+  print(ftest(s))
